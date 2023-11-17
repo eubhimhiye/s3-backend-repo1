612bbacdae8d889be792b15d6fb27b0b0f35b451
Emmanuel Ubhimhiye <emmbest22@yahoo.com>
2023-11-17 05:36:05 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+resource "aws_s3_bucket" "backend" {
+  bucket = "bootcamp32-${lower(var.env)}-${random_integer.backend.result}"
+
+  tags = {
+    Name        = "My backend"
+    Environment = "Dev"
+  }
+}
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "backend" {
+  bucket = aws_s3_bucket.backend.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+resource "random_integer" "backend" {
+  min = 1
+  max = 100
+  keepers = {
+    # Generate a new integer each time we switch to a new listener ARN
+    Environment = var.env
+  }
+}
